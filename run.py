d93cddbfca395a9264ed7fe7fe83ec9785a2f3c6
Nicole Burmann <daria215@gmail.com>
2024-08-28 06:57:06 +0000

    Add welcome message and ask user for name

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def game():
+    """
+    Welcome message and start of game.
+    Get name from user.
+    """
+    print("Welcome to a virtual hike.")
+    print("We are going to hike 50km today.")
+    print("Are you ready to hike?")
+    print("But first, what is your name? ")
+    name = input()
+    capitalized_name = name.capitalize()
+    print(f"Welcome, {capitalized_name}. Let's get ready to hike.\n")
+    startingPoint()
+
+game()
